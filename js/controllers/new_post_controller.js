5087f77c0b10a389e409df7efc99360b8a267b62
lsprats <lsp.prats@gmail.com>
2015-05-30 03:37:01 -0300

    final app

--- js/controllers/new_post_controller.js
+++ js/controllers/new_post_controller.js
@@ -1,12 +1,15 @@
 app.controller("NewPostController", function($scope, PostResource, $location){
 
 	$scope.post = {};
-	$scope.title = "New Post"
+	$scope.title = "New Post";
+
 	$scope.savePost = function(){
+    
 		PostResource.save({data: $scope.post}, function(data){
 			console.log(data);
 			$location.path("/");
 		});
+
 	}
 
 });
--- js/controllers/post_controller.js
+++ js/controllers/post_controller.js
@@ -1,13 +1,15 @@
 app.controller("PostController", function($scope, PostResource, $routeParams, $location){
 
-	$scope.title = "Edit Post"
+	$scope.title = "Edit Post";
 	$scope.post = PostResource.get({id: $routeParams.id});
 	
 	$scope.savePost = function(){
+
 		PostResource.update({id: $scope.post.id},{data: $scope.post}, function(data){
 			console.log(data);
 			$location.path("/post/"+$scope.post.id);
 		});
-	}
+	
+  }
 
 });
