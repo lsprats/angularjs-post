7f0978456132eb24c09d2361724de57b586e5857
lsprats <lsp.prats@gmail.com>
2015-05-30 03:25:45 -0300

    final app

--- js/controllers/main_controller.js
+++ js/controllers/main_controller.js
@@ -2,6 +2,7 @@ app.controller("MainController", function($scope, $resource, PostResource, UserR
 	
 	$scope.posts = PostResource.query();
 	$scope.users = UserResource.query();
+  
 	$scope.removePost = function(post){
 		PostResource.delete({id: post.id});
 		$scope.posts = $scope.posts.filter(function(element){
